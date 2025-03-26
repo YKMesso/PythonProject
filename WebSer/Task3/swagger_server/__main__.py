c8d9d2c7818f5e785158ceb58b151fe04f365492
Emmanuel O <B00142467@mytudublin.ie>
2025-03-26 23:41:41 +0000

    task3 95%

--- WebSer/Task3/swagger_server/__main__.py
+++ WebSer/Task3/swagger_server/__main__.py
@@ -7,9 +7,9 @@ from swagger_server import encoder
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = encoder.JSONEncoder
+    app.app.json_provider_class = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'API Title'}, pythonic_params=True)
-    app.run(port=8080)
+    app.run(port=8001)
 
 
 if __name__ == '__main__':
