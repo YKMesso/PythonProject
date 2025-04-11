659383fb9e3ae30238b63339e2bcafbf4f494f60
Emmanuel O <B00142467@mytudublin.ie>
2025-04-11 03:53:50 +0100

    _

--- CompVis/Assignment2/pneumonia_classification.py
+++ CompVis/Assignment2/pneumonia_classification.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Rescaling, BatchNormalization
-from keras.optimizers import RMSprop,Adam
+from keras.optimizers import RMSprop, Adam
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,8 +15,9 @@ from collections import Counter
 from keras.src.layers import RandomRotation
 from sklearn.utils import class_weight
 from keras.layers import RandomFlip, RandomRotation, RandomZoom
+import keras_tuner as kt
 
-#from 'C:\\Users\\messo\\PycharmProjects\\PythonProject\\CompVis\\Assignment2\\mnist_classification' import y_train
+# from 'C:\\Users\\messo\\PycharmProjects\\PythonProject\\CompVis\\Assignment2\\mnist_classification' import y_train
 
 
 batch_size = 12
@@ -25,12 +26,13 @@ epochs = 8
 img_width = 128
 img_height = 128
 img_channels = 3
-fit = True #make fit false if you do not want to train the network again
+fit = True  # make fit false if you do not want to train the network again
 
 train_dir = 'C:\\Users\\messo\\PycharmProjects\\PythonProject\\CompVis\\Assignment2\\chest_xray\\train'
 test_dir = 'C:\\Users\\messo\\PycharmProjects\\PythonProject\\CompVis\\Assignment2\\chest_xray\\test'
 
-#Check if balanced
+
+# Check if balanced
 def count_images(dir):
     class_counts = {}
     for class_name in os.listdir(dir):
@@ -40,19 +42,18 @@ def count_images(dir):
             class_counts[class_name] = count
     return class_counts
 
+
 train_count = count_images(train_dir)
 test_count = count_images(test_dir)
 
 print("Training Set Distribution:", train_count)
 print("Test Set Distribution:", test_count)
 
-
 #
 
 with tf.device('/gpu:0'):
-
-    #create training,validation and test datatsets
-    train_ds,val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    # create training,validation and test datatsets
+    train_ds, val_ds = tf.keras.preprocessing.image_dataset_from_directory(
         train_dir,
         seed=123,
         validation_split=0.2,
@@ -71,7 +72,7 @@ with tf.device('/gpu:0'):
         shuffle=True)
 
     class_names = train_ds.class_names
-    print('Class Names: ',class_names)
+    print('Class Names: ', class_names)
     num_classes = len(class_names)
 
     plt.figure(figsize=(10, 10))
@@ -83,42 +84,49 @@ with tf.device('/gpu:0'):
             plt.axis("off")
     plt.show()
 
-
-
     data_augmentation = tf.keras.Sequential([
         RandomFlip("horizontal"),
         RandomRotation(0.1),
         RandomZoom(0.1)
     ])
 
+    for images, _ in train_ds.take(1):
+        plt.figure(figsize=(10, 10))
+        for i in range(9):
+            augmented_image = data_augmentation(images[i:i + 1])  # Apply augmentation to a single image
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(augmented_image[0].numpy().astype("uint8"))
+            plt.axis("off")
+        plt.suptitle("Augmented Training Images")
+        plt.show()
 
-    #create model
+    # create model
     model = tf.keras.models.Sequential([
         data_augmentation,
-        Rescaling(1.0/255),
-        Conv2D(16, (3,3), activation = 'relu', input_shape = (img_height,img_width, img_channels)),
-        MaxPooling2D(2,2),
-        Conv2D(32, (3,3), activation = 'relu'),
-        MaxPooling2D(2,2),
-        Conv2D(32, (3,3), activation = 'relu'),
-        MaxPooling2D(2,2),
-        Flatten(), # flatten multidimensional outputs into single dimension for input to dense fully connected layers
-        Dense(512, activation = 'relu'),
+        Rescaling(1.0 / 255),
+        Conv2D(16, (3, 3), activation='relu', input_shape=(img_height, img_width, img_channels)),
+        MaxPooling2D(2, 2),
+        Conv2D(32, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+        Conv2D(32, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+        Flatten(),  # flatten multidimensional outputs into single dimension for input to dense fully connected layers
+        Dense(512, activation='relu'),
         Dropout(0.2),
-        Dense(num_classes, activation = 'softmax')
+        Dense(num_classes, activation='softmax')
     ])
 
     model.compile(loss='sparse_categorical_crossentropy',
                   optimizer=Adam(),
                   metrics=['accuracy'])
 
-    #earlystop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=5)
-    save_callback = tf.keras.callbacks.ModelCheckpoint("pneumonia.keras",save_freq='epoch',save_best_only=True)
+    # earlystop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=5)
+    save_callback = tf.keras.callbacks.ModelCheckpoint("pneumonia.keras", save_freq='epoch', save_best_only=True)
 
-    #Class weighting
+    # Class weighting
     y_train = np.concatenate([y.numpy() for x, y in train_ds], axis=0)
 
-    class_weights = class_weight.compute_class_weight(class_weight= 'balanced', classes = np.unique(y_train), y=y_train)
+    class_weights = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
 
     class_weights_dict = {int(k): round(float(v), 2) for k, v in enumerate(class_weights)}
     print("Class Weights:", class_weights_dict)
@@ -134,10 +142,9 @@ with tf.device('/gpu:0'):
     else:
         model = tf.keras.models.load_model("pneumonia.keras")
 
-    #if shuffle=True when creating the dataset, samples will be chosen randomly
+    # if shuffle=True when creating the dataset, samples will be chosen randomly
     score = model.evaluate(test_ds, batch_size=batch_size)
     print('Test accuracy:', score[1])
-
 
     if fit:
         plt.plot(history.history['accuracy'])
@@ -146,15 +153,15 @@ with tf.device('/gpu:0'):
         plt.ylabel('accuracy')
         plt.xlabel('epoch')
         plt.legend(['train', 'val'], loc='upper left')
-        
+
     test_batch = test_ds.take(1)
     plt.figure(figsize=(10, 10))
     for images, labels in test_batch:
         for i in range(6):
             ax = plt.subplot(2, 3, i + 1)
             plt.imshow(images[i].numpy().astype("uint8"))
-            prediction = model.predict(tf.expand_dims(images[i].numpy(),0))#perform a prediction on this image
-            plt.title('Actual:' + class_names[labels[i].numpy()]+ '\nPredicted:{} {:.2f}%'.format(class_names[np.argmax(prediction)], 100 * np.max(prediction)))
+            prediction = model.predict(tf.expand_dims(images[i].numpy(), 0))  # perform a prediction on this image
+            plt.title('Actual:' + class_names[labels[i].numpy()] + '\nPredicted:{} {:.2f}%'.format(
+                class_names[np.argmax(prediction)], 100 * np.max(prediction)))
             plt.axis("off")
     plt.show()
-
